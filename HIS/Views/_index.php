05244379aa71a819ee9317e98d8d48ab8515c601
arditcurri12 <129061270+arditcurri12@users.noreply.github.com>
2023-05-01 00:42:53 +0200

    created index page

--- HIS/Views/_index.php
+++ HIS/Views/_index.php
@@ -1,1 +1,151 @@
-<h1>Index Page e ndryshuarrrr</h1>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+
+
+  <title>Hospital Menagment</title>
+
+  <link rel="stylesheet" href="assets/css/maicons.css">
+  <link rel="stylesheet" href="assets/css/bootstrap.css">
+  <link rel="stylesheet" href="assets/vendor/owl-carousel/css/owl.carousel.css">
+  <link rel="stylesheet" href="assets/vendor/animate/animate.css">
+  <link rel="stylesheet" href="assets/css/theme.css">
+</head>
+
+<body>
+
+
+  <div class="back-to-top"></div>
+
+
+  <nav class="navbar navbar-expand-lg navbar-light shadow-sm">
+    <div class="container">
+      <a class="navbar-brand" href="#"><span class="text-primary">Hospital</span> Menagment</a>
+
+
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupport"
+        aria-controls="navbarSupport" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarSupport">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="index.html">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="">About Us</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="">Doctors</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="">News</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="">Contact</a>
+          </li>
+          <li class="nav-item">
+            <a class="btn btn-primary ml-lg-3" href="">Login / Register</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  </header>
+
+  <div class="page-hero bg-image overlay-dark" style="background-image: url(assets/img/bg_image_1.jpg);">
+    <div class="hero-section">
+      <div class="container text-center wow zoomIn">
+        <span class="subhead">Ta bejmë jetë tënde te lumtur!</span>
+        <h1 class="display-4">Jeto Shëndetshëm</h1>
+        <a href="#" class="btn btn-primary">Le te konsultohemi</a>
+      </div>
+    </div>
+  </div>
+
+
+  <div class="bg-light">
+    <div class="page-section py-3 mt-md-n5 custom-index">
+      <div class="container">
+        <div class="row justify-content-center">
+          <div class="col-md-4 py-3 py-md-0">
+            <div class="card-service wow fadeInUp">
+              <div class="circle-shape bg-secondary text-white">
+                <span class="mai-chatbubbles-outline"></span>
+              </div>
+              <p><span>Kontakto</span> me mjeket</p>
+            </div>
+          </div>
+          <div class="col-md-4 py-3 py-md-0">
+            <div class="card-service wow fadeInUp">
+              <div class="circle-shape bg-primary text-white">
+                <span class="mai-shield-checkmark"></span>
+              </div>
+              <p><span>Mbrojtja</span> jonë</p>
+            </div>
+          </div>
+          <div class="col-md-4 py-3 py-md-0">
+            <div class="card-service wow fadeInUp">
+              <div class="circle-shape bg-accent text-white">
+                <span class="mai-basket"></span>
+              </div>
+              <p><span>Farmacia</span> jonë</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="page-section pb-0">
+      <div class="container">
+        <div class="row align-items-center">
+          <div class="col-lg-6 py-3 wow fadeInUp">
+            <h1>Mirë se vini ne qendren e<br> shendetit</h1>
+            <p class="text-grey mb-4">Të qëndrosh i shëndetshëm ose të bëhesh i shëndetshëm do të thotë të mos dëmtosh
+              ose të rivendosësh aftësitë vetëshëruese të trupit (sistemi imunitar, homeostaza). Të gjitha organet e
+              sistemit imun, qelizat e tyre, antitrupat, inflamacionet, ethet etj. së bashku formojnë sistemin imunitar
+              të trupit apo edhe fuqin vetëshëruese të trupit</p>
+            <a href="" class="btn btn-primary">Learn More</a>
+          </div>
+          <div class="col-lg-6 wow fadeInRight" data-wow-delay="400ms">
+            <div class="img-place custom-img-1">
+              <img src="assets/img/bg-doctor.png" alt="">
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <footer class="page-footer">
+    <div class="container">
+      <div class="row px-md-3">
+
+        <div class="col-sm-6 col-lg-3 py-3">
+          <h5>Contact</h5>
+          <p class="footer-link mt-2">Kosovë Prishtinë 10000</p>
+          <a href="#" class="footer-link">+383 49 000 000</a>
+          <a href="#" class="footer-link">hospitalmenagment@universitetiaab.com</a>
+
+
+
+        </div>
+      </div>
+
+      <hr>
+
+      <p id="copyright">Copyright &copy; 2023 <a href="https://aab-edu.net/" target="_blank">Kolegji AAB</a>. All right
+        reserved</p>
+    </div>
+  </footer>
+
+</body>
+
+</html>
