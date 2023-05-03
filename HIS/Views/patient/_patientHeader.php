470c156b62d4f3d05975b56678ef6aa30d2b80fc
arditcurri12 <129061270+arditcurri12@users.noreply.github.com>
2023-05-03 11:52:20 +0200

    f

--- HIS/Views/patient/_patientHeader.php
+++ HIS/Views/patient/_patientHeader.php
@@ -50,7 +50,7 @@ $vals = array(
     <link rel="stylesheet" href="css/login.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
           integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-    <link rel="stylesheet" href="assets/css/Registration.css">
+
     <link rel="stylesheet" href="assets/css/maicons.css">
     <link rel="stylesheet" href="assets/css/bootstrap.css">
 
